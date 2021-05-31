f7e191087307d7775c2aafbed50220a95676aa1b
Prappo Prince <prappo.prince@gmail.com>
2021-05-31 18:35:36 +0600

    added manifest to vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+    build: {
+        // generate manifest.json in outDir
+        manifest: true,
+
+    },
+    plugins: [vue()]
 })
